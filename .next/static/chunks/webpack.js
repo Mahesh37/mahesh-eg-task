0d29a3c5ecf609deb9471b7be430255d34e62ef1
Mahesh Mamidala <maheshmamidala@Maheshs-Air.Dlink>
2022-10-11 22:32:50 +0530

    added gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22731a79b07a6357"; }
+/******/ 		__webpack_require__.h = function() { return "91d80159d0b7624c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
